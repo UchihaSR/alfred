e93dfde422dae130ea78f4d5895c9e6fb4446ccf
Salman Abedin <salmanabedin@zohomail.com>
2020-06-24 06:08:05 +0600

    Added a pro tip that most of you guys seemed to miss

--- launch-devour.sh
+++ launch-devour.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-# File launch script
+# XDG Killer
+# Launches files based on their mimetype
 
 echo "$*" | grep "\.ar\." && {
     devour alacritty \
@@ -8,38 +9,29 @@ echo "$*" | grep "\.ar\." && {
         -e "$EDITOR" "$*" &
     exit
 }
-
 echo "$1" | grep "\.sent$" && {
     devour sent "$1" &
     exit
 }
-
 case $(file --mime-type "$*" -bL) in
-
     text/* | inode/x-empty | application/json | application/octet-stream)
         "$EDITOR" "$*"
         ;;
-
     video/*)
         pidof mpv || devour mpv "$*"
         ;;
-
     application/pdf | application/postscript)
         pidof zathura || devour zathura "$*"
         ;;
-
     image/gif)
         pgrep mpv || devour "mpv --loop" "$*"
         ;;
-
     image/*)
         pidof feh ||
             devour \
                 "feh -A 'set-bg %f' -B 'black' -F -d --edit --keep-zoom-vp --start-at" "$*"
         ;;
-
     application/zip)
         unzip "$*" -d "${1%.*}"
         ;;
-
 esac
