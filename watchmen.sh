eee47bf770662b5770071c63c10064437525ff92
Salman Abedin <salmanabedin@zohomail.com>
2020-06-26 03:36:40 +0600

    added checkstorage script

--- watchmen.sh
+++ watchmen.sh
@@ -10,7 +10,7 @@ while :; do
             $GIT/own/private \
             "
             inotifywait -m -r -e create,moved_to $dirs |
-                while read -r; do
+                while read -r line; do
                     cp -frsu -t ~ \
                         "$GIT"/own/magpie/. \
                         "$GIT"/own/private/.
@@ -19,7 +19,7 @@ while :; do
         --mails)
             path=~/.local/share/mail/gmail/INBOX/new
             inotifywait -m -e move $path |
-                while read -r; do
+                while read -r line; do
                     refresh-block 2
                 done &
             ;;
