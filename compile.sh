50001853fce3b1e73fc6206f5d04fb421637fdfb
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-06-27 18:00:02 +0600

    Added extract script

--- compile.sh
+++ compile.sh
@@ -20,6 +20,7 @@ case $ext in
     h | sh) doas make install ;;
     py) python "$path" ;;
     tex) xelatex "$path" ;;
+    c) gcc -Wall "$path" -lxcb ;;
         # c) cc "$path" ;;
         # c) cc "$path" -o "$name" && "$name" ;;
         # ms) groff -ms -ept -K utf8 "$path" > "$name".ps ;;
