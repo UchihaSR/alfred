b788b0e3255f5a9377fa7a8338b032267a0731c0
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-06-28 11:33:35 +0600

    Added options for syncing mozilla

--- mirror.sh
+++ mirror.sh
@@ -19,8 +19,8 @@ while :; do
                     # git pull
                     git add .
                     [ -z "$(git status --porcelain)" ] && continue
-                    # [ "$PWD" = /home/git/own/firefox ] ||
-                    message=$(timeout 15 rofi -dmenu -i -p "$(echo "$PWD" | awk -F / '{print $NF}')")
+                    [ "$PWD" = /home/git/own/firefox ] ||
+                        message=$(timeout 15 rofi -dmenu -i -p "$(echo "$PWD" | awk -F / '{print $NF}')")
                     [ "$message" ] || message=$(git log -1 | tail -1 | awk '{$1=$1};1')
                     git commit -m "$message" && git push
                 fi
