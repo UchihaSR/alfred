528e38da0648d7c26fb277cdf774c780bc0e6346
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-06-30 13:00:09 +0600

    Added firefox mirroring

--- mirror.sh
+++ mirror.sh
@@ -14,11 +14,11 @@ notify-send -i "$ICONS/mirror.png" "Mirroring now"
 
 while :; do
     case $1 in
-        --firefox)
+        --firefox | -f)
             rsync -a --delete ~/.mozilla/firefox/"$FIREFOXPROFILE".default-release \
                 "$GIT"/own/firefox/.mozilla/firefox
             ;;
-        --git)
+        --git | -g)
             for dir in "$GIT"/own/*/; do
                 if [ -d "$dir" ]; then
                     cd "$dir" || exit 1
@@ -35,23 +35,23 @@ while :; do
             # wait &
             # notify-send -t 3000 -i "$ICONS"/gitlab.png "Done syncing git repos"
             ;;
-        --mail)
+        --mail | -m)
             mbsync -c ~/.config/isync/mbsyncrc -a
             unread=$(find ~/.local/share/mail/gmail/INBOX/new/* -type f 2> /dev/null | wc -l)
             [ "$unread" -gt 0 ] && notify-send -t 0 -i "$ICONS/mail.png" \
                 "You've got $unread new mail!"
             ;;
-        --calcurse)
+        --calcurse | -c)
             CALCURSE_CALDAV_PASSWORD=$(gpg -d --batch --passphrase asdlkj ~/.local/share/passwords/salmanabedin@disroot.org.gpg) calcurse-caldav
             # --init=keep-remote
             # notify-send -i "$ICONS"/calendar.png "Done syncing calcurse"
             ;;
-        --arch)
+        --arch | -a)
             doas -- pacman -Syyu --noconfirm
             yay -Syyu --noconfirm
             npm update -g
             ;;
-        --phone)
+        --phone | -p)
             ANDROIDMOUNT=/mnt/android
             ANDROIDDISK=/home/phone
             # notify-send -t 3000 -i "$ICONS"/phone.png "Phone Sync" "Time to sync"
