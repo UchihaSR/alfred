9e0c037491ef0ef7bea271ce15f1489aaede259c
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-06-30 21:18:02 +0600

    Added firefox mirroring

--- toggle.sh
+++ toggle.sh
@@ -5,14 +5,14 @@
 
 while :; do
     case $1 in
-        --wall-reel)
+        --wall-reel | -r)
             REELPID=/tmp/reelpid
             if [ -s $REELPID ]; then
                 kill -9 "$(cat $REELPID)"
                 : > $REELPID
-                notify-send -i "$ICONS"/wall.png "Wallpaper Reeling Stopped"
+                notify-send -u low -i "$ICONS"/wall.png "Wallpaper Reeling Stopped"
             else
-                notify-send -i "$ICONS"/wall.png "Wallpaper Reeling Started"
+                notify-send -u low -i "$ICONS"/wall.png "Wallpaper Reeling Started"
                 while :; do
                     setdisplay --bg shuffle
                     sleep 5m
@@ -20,27 +20,27 @@ while :; do
                 echo $! > $REELPID
             fi
             ;;
-        --noti)
+        --noti | -n)
             if [ -s "$DONT_DISTURB_MODE" ]; then
                 : > "$DONT_DISTURB_MODE"
                 killall -SIGUSR2 dunst
                 sleep 1
-                notify-send -t 1000 -i "$ICONS"/bell.png 'Disturb all you want'
+                notify-send -u low -i "$ICONS"/bell.png 'Disturb all you want'
             else
                 echo on > "$DONT_DISTURB_MODE"
-                notify-send -t 1000 -i "$ICONS"/dnd.png 'Do not disturb'
+                notify-send -u low -i "$ICONS"/dnd.png 'Do not disturb'
                 sleep 2
                 killall -SIGUSR1 dunst
             fi
             refresh-block 3
             ;;
-        --wifi)
+        --wifi | -w)
             if pidof iwd; then
                 doas systemctl stop iwd
-                notify-send -i "$ICONS"/disconnected.png 'Turned Wifi Off'
+                notify-send -u low -i "$ICONS"/disconnected.png 'Turned Wifi Off'
             else
                 doas systemctl start iwd
-                notify-send -i "$ICONS"/connected.png 'Turned Wifi On'
+                notify-send -u low -i "$ICONS"/connected.png 'Turned Wifi On'
             fi
             ;;
         *) break ;;
