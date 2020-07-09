4d0d4fc6dbf87c6d4b9c5282ede8512a04d1b302
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-09 13:23:06 +0600

    Added qmedia script

--- launch.sh
+++ launch.sh
@@ -70,7 +70,8 @@ case $1 in
                 # testt mpv "$*"
                 ;;
             application/pdf | application/postscript)
-                pidof zathura || run zathura "$*"
+                # pidof zathura || run zathura "$*"
+                devour zathura "$*"
                 ;;
             image/*)
                 pidof feh ||
