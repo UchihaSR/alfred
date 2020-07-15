7a9080cf5162ffb9e690416e6db10bfe5c6fd6f4
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-16 02:46:23 +0600

    Added backlight script

--- housekeep.sh
+++ housekeep.sh
@@ -2,7 +2,6 @@
 
 doas -- pacman -Scc --noconfirm
 doas -- pacman -Rns "$(pacman -Qtdq)" --noconfirm
-doas -- rm -fr ~/.local/share/cache/* /var/log/journal/* ~/.local/share/Trash/.*
+doas -- rm -fr ~/.local/share/cache/* /var/log/journal/* ~/.local/share/Trash/*
 doas -- find ~ -xtype l -delete
 doas -- find ~ -type d -empty -delete
-
