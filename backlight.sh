d5c945f298ca38a05a77b8046cc199597442486a
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-18 01:15:41 +0600

    Added backlight script

--- backlight.sh
+++ backlight.sh
@@ -3,19 +3,17 @@
 # Modulates backlight level
 
 DEVICE=/sys/class/backlight/"$(ls /sys/class/backlight)"
-CURRENT="$(cat "$DEVICE"/brightness)"
-MAX="$(cat "$DEVICE"/max_brightness)"
-MARGIN="$((MAX * 10 / 100))"
+CURRENT=$(cat "$DEVICE"/brightness)
+MAX=$(cat "$DEVICE"/max_brightness)
+MARGIN=$((MAX * 10 / 100))
 
 case $1 in
     --up)
         increased=$((CURRENT + MARGIN))
-        # [ "$increased" -gt "$MAX" ] && increased="$MAX"
-        (increased > MAX) && increased="$MAX"
+        [ "$increased" -gt "$MAX" ] && increased="$MAX"
         echo "$increased" > "$DEVICE"/brightness
         ;;
     --down)
-        DECREASED=$((CURRENT - MARGIN))
-        echo "$DECREASED" > "$DEVICE"/brightness
+        echo $((CURRENT - MARGIN)) > "$DEVICE"/brightness
         ;;
 esac
