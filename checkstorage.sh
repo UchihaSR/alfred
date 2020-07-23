c7deff34b84975b30e62a63a33d5b1d1f8ffdbf8
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-23 18:02:38 +0600

    Added backlight script

--- checkstorage.sh
+++ checkstorage.sh
@@ -7,7 +7,7 @@ while [ -n "$1" ]; do
     LOCATION=$1
     THRESHOLD=$2
     FREE=$(df -h "$LOCATION" | awk '(NR == 2){print int($4)}' | sed 's/G//')
-    [ "$FREE" -lt "$THRESHOLD" ] && notify-send -t 1000 -i "$ICONS"/critical.png \
+    [ "$FREE" -lt "$THRESHOLD" ] && notify-send -t 0 -i "$ICONS"/critical.png \
         "$LOCATION has less than $THRESHOLD gig"
     shift 2
 done
