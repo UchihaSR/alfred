963ead5a09c7933858637b57b2f7afb770f50fa1
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-25 05:15:14 +0600

    Added backlight script

--- setdisplay.sh
+++ setdisplay.sh
@@ -5,59 +5,59 @@
 #   Requires an Environment variable named WALLPAPERS that has the path to your wallpapers
 # setdisplay --dpi sets the correct dpi
 
-while :; do
-    case $1 in
-        --bg)
-            WALL=/tmp/wall
-            shift
-            case $1 in
-                shuffle)
-                    find "$WALLPAPERS" -name "*.jpg" -o -name "*.png" | shuf -n1 > "$WALL"
-                    feh --no-fehbg --bg-scale "$(cat $WALL)"
-                    ;;
-                delete)
-                    find "$WALLPAPERS" -name "$(cat $WALL)" -delete
-                    $0 --bg shuffle
-                    ;;
-                *)
-                    echo "$1" > $WALL
-                    feh --no-fehbg --bg-scale "$(cat $WALL)"
-                    ;;
-            esac
-            ;;
-        --dpi)
-            FILE=~/.config/X11/Xresources
+case $1 in
+    --bg)
+        exec 3> /tmp/wall
+        exec 4< /tmp/wall
+        case $1 in
+            shuffle)
+                find "$WALLPAPERS" -name "*.jpg" -o -name "*.png" | shuf -n1 >&3
+                feh --no-fehbg --bg-scale "$(cat <&4)"
+                ;;
+            delete)
+                find "$WALLPAPERS" -name "$(cat <&4)" -delete
+                $0 --bg shuffle
+                ;;
+            *)
+                echo "$1" >&3
+                feh --no-fehbg --bg-scale "$(cat <&4)"
+                ;;
+        esac
+        exec 3<&-
+        exec 4<&-
+        ;;
+    --dpi)
+        FILE=~/.config/X11/Xresources
 
-            grep 'Xft.dpi' "$FILE" && xrdb -merge "$FILE" && return
+        grep 'Xft.dpi' "$FILE" && xrdb -merge "$FILE" && return
 
-            INFO=$(xrandr | grep ' connected')
+        INFO=/tmp/display_info
+        xrandr | grep ' connected' > $INFO
 
-            WIDTH_MM=$(echo "$INFO" | awk '{print $NF}')
-            WIDTH_IN=$(echo "${WIDTH_MM%mm}" | awk '{print $1 * 0.03937007874015748}')
+        WIDTH_MM=$(awk '{print $NF}' $INFO)
+        WIDTH_IN=$(echo "${WIDTH_MM%mm}" | awk '{print $1 * 0.03937007874015748}')
 
-            HEIGHT_MM=$(echo "$INFO" | awk '{print $(NF-2)}')
-            HEIGHT_IN=$(echo "${HEIGHT_MM%mm}" | awk '{print $1 * 0.03937007874015748}')
+        HEIGHT_MM=$(awk '{print $(NF-2)}' $INFO)
+        HEIGHT_IN=$(echo "${HEIGHT_MM%mm}" | awk '{print $1 * 0.03937007874015748}')
 
-            DIAGONAL_IN=$(awk -v w="$WIDTH_IN" -v h="$HEIGHT_IN" \
-                'BEGIN{print sqrt(w^2 + h^2)}')
+        DIAGONAL_IN=$(awk -v w="$WIDTH_IN" -v h="$HEIGHT_IN" \
+            'BEGIN{print sqrt(w^2 + h^2)}')
 
-            PX=$(echo "$INFO" | cut -d ' ' -f 4)
+        PX=$(cut -d ' ' -f 4 $INFO)
 
-            WIDTH_PX=${PX%x*}
+        WIDTH_PX=${PX%x*}
 
-            HEIGHT_PXS=${PX#*x}
-            HEIGHT_PX=${HEIGHT_PXS%%+*}
+        TEMP_HEIGHT_PX=${PX#*x}
+        HEIGHT_PX=${TEMP_HEIGHT_PX%%+*}
 
-            DIAGONAL_PX=$(awk -v w="$WIDTH_PX" -v h="$HEIGHT_PX" \
-                'BEGIN{print sqrt(w^2 +h^2)}')
+        DIAGONAL_PX=$(awk -v w="$WIDTH_PX" -v h="$HEIGHT_PX" \
+            'BEGIN{print sqrt(w^2 +h^2)}')
 
-            DPI=$(awk -v dp="$DIAGONAL_PX" -v di="$DIAGONAL_IN" \
-                'BEGIN{print dp / di}')
+        DPI=$(awk -v dp="$DIAGONAL_PX" -v di="$DIAGONAL_IN" \
+            'BEGIN{print dp / di}')
 
-            echo "Xft.dpi: $DPI" >> "$FILE"
-            xrdb -merge "$FILE"
-            ;;
-        *) break ;;
-    esac
-    shift
-done
+        echo "Xft.dpi: $DPI" >> "$FILE"
+        xrdb -merge "$FILE"
+        rm $INFO
+        ;;
+esac
