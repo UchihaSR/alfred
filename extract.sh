e1f19c877b9d73637348a3f4c308c50e96be0231
salman-abedin <salman104alvi@gmail.com>
2020-08-02 09:23:28 +0600

    Added backlight script

--- extract.sh
+++ extract.sh
@@ -10,12 +10,12 @@ case $ext in
     zip) unzip "$1" -d "${1%.*}" ;;
     tar) tar -xf "$1" -C "${1%.*}" ;;
     xz) unxz "$1" ;;
-    gz) tar -xzf "$1" -C "${1%.*}" ;;
+    gz) gunzip "$1" ;;
+    # gz) tar -xzf "$1" -C "${1%.*}" ;;
     rar) unrar "$1" ;;
     7z) 7z x "$1" ;;
     *) exit 1 ;;
         # tar) tar -xvf "$1" ;;
-        # gz) gunzip "$1" ;;
         # zip) unzip "$path" -d "${1%.*}" ;;
 esac
 
