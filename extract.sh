b2ef71f06f927da8716ca8e9611739a6f94a703d
salman-abedin <salman104alvi@gmail.com>
2020-08-12 22:09:55 +0600

    Added backlight script

--- extract.sh
+++ extract.sh
@@ -7,16 +7,17 @@
 ext="${1##*.}"
 
 case $ext in
-    zip) unzip "$1" -d "${1%.*}" ;;
-    tar) tar -xf "$1" -C "${1%.*}" ;;
-    xz) unxz "$1" ;;
-    gz) gunzip "$1" ;;
-    # gz) tar -xzf "$1" -C "${1%.*}" ;;
-    rar) unrar "$1" ;;
-    7z) 7z x "$1" ;;
-    *) exit 1 ;;
-        # tar) tar -xvf "$1" ;;
-        # zip) unzip "$path" -d "${1%.*}" ;;
+   zip) unzip "$1" -d "${1%.*}" ;;
+   tar) tar -xf "$1" -C "${1%.*}" ;;
+   xz) unxz "$1" ;;
+   gz) gunzip "$1" ;;
+   bz2) tar -xjvf "$1" ;;
+   rar) unrar "$1" ;;
+   7z) 7z x "$1" ;;
+   *) exit 1 ;;
+   # gz) tar -xzf "$1" -C "${1%.*}" ;;
+      # tar) tar -xvf "$1" ;;
+      # zip) unzip "$path" -d "${1%.*}" ;;
 esac
 
 [ "$clean" ] && mv -f "$1" ~/.local/share/Trash
