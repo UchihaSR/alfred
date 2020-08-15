66726bd0e21fe9cfba3160974b14423b4724d903
salman-abedin <salman104alvi@gmail.com>
2020-08-15 15:43:14 +0600

    Added backlight script

--- screensaver.sh
+++ screensaver.sh
@@ -10,13 +10,13 @@ dur=$((INACTIVITY / 60))
 # timeout 30m xterm -fullscreen -e "sleep 1; cmatrix -s -b -u 10" || systemctl suspend
 # timeout 30m xterm -fullscreen -e "sleep 1; rain" || systemctl suspend
 
-xdo hide -a yabar
+# xdo hide -a yabar
 timeout "$dur"m \
    ffplay \
    -volume 0 -exitonkeydown -exitonmousedown -loop "$dur" \
    -fs ~/.local/share/screensavers/matrix.mp4 \
    > /dev/null 2>&1 ||
    systemctl suspend
-xdo show -a yabar
+# xdo show -a yabar
 
 # timeout 300 mpv -fs --no-osc --no-audio --loop=inf --no-resume-playback ~/.wallpapers/matrix.mp4 >/dev/null 2>&1 || systectl suspend
