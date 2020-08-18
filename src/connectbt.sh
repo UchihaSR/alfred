96ce1138cfd8d84477f72be348af26faddeb2185
salman-abedin <salman104alvi@gmail.com>
2020-08-18 11:34:06 +0600

    Added backlight script

--- src/connectbt.sh
+++ src/connectbt.sh
@@ -6,11 +6,11 @@ doas rfkill unblock bluetooth
 doas systemctl start bluetooth
 bluetoothctl power on
 
-# right=${1:-E8:EC:A3:27:60:15}
+right=${1:-E8:EC:A3:27:60:15}
 left=${1:-E8:EC:A3:15:05:0D}
 
-# bluetoothctl connect "$right" ||
-bluetoothctl connect "$left"
+bluetoothctl connect "$right" ||
+   bluetoothctl connect "$left"
 
 # bluetoothctl scan off
 
