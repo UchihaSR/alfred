184ace58ae6aadde9d93f856332629c37206767a
salman-abedin <salman104alvi@gmail.com>
2020-08-24 11:11:58 +0600

    Added backlight script

--- src/launch.sh
+++ src/launch.sh
@@ -5,18 +5,8 @@
 run() { setsid "$@" > /dev/null 2>&1 & }
 
 launch_dwm() {
-   # relaunch DWM if the binary changes, otherwise bail
-   csum=$(sha1sum $(which dwm))
-   new_csum=""
-   while true; do
-      if [ "$csum" != "$new_csum" ]; then
-         csum=$new_csum
-         dwm
-      else
-         exit 0
-      fi
-      new_csum=$(sha1sum $(which dwm))
-      sleep 0.5
+   while :; do
+      dwm 2> ~/.dwm.log
    done
 }
 
